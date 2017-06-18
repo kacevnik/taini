55b31a01fa4581377b1cac54c83a966e1892f4fe
Dmitrii Kovalev <kacevnik@yandex.ru>
2017-06-18 20:25:33 +0300

    Добавил кнопки расшаривания Яндекса и изменил стили

--- sidebar-left.php
+++ sidebar-left.php
@@ -7,16 +7,9 @@
 		<?php if(get_theme_option('socialnetworks') != '') {
 			?>
     			<div class="addthis_toolbox">   
-    			    <div class="custom_images">
-    			            <a rel="nofollow" class="addthis_button_twitter"><img src="<?php bloginfo('template_directory'); ?>/images/socialicons/twitter.png" width="32" height="32" alt="Twitter" /></a>
-    			            <a rel="nofollow" class="addthis_button_delicious"><img src="<?php bloginfo('template_directory'); ?>/images/socialicons/delicious.png" width="32" height="32" alt="Delicious" /></a>
-    			            <a rel="nofollow" class="addthis_button_facebook"><img src="<?php bloginfo('template_directory'); ?>/images/socialicons/facebook.png" width="32" height="32" alt="Facebook" /></a>
-    			            <a rel="nofollow" class="addthis_button_digg"><img src="<?php bloginfo('template_directory'); ?>/images/socialicons/digg.png" width="32" height="32" alt="Digg" /></a>
-    			            <a rel="nofollow" class="addthis_button_stumbleupon"><img src="<?php bloginfo('template_directory'); ?>/images/socialicons/stumbleupon.png" width="32" height="32" alt="Stumbleupon" /></a>
-                            <a rel="nofollow" class="addthis_button_favorites"><img src="<?php bloginfo('template_directory'); ?>/images/socialicons/favorites.png" width="32" height="32" alt="Favorites" /></a>
-    			            <a class="addthis_button_more"><img src="<?php bloginfo('template_directory'); ?>/images/socialicons/more.png" width="32" height="32" alt="More" /></a>
-    			    </div>
-    			    <script type="text/javascript" src="http://s7.addthis.com/js/250/addthis_widget.js?pub=xa-4a65e1d93cd75e94"></script>
+<script src="//yastatic.net/es5-shims/0.0.2/es5-shims.min.js"></script>
+<script src="//yastatic.net/share2/share.js"></script>
+<div class="ya-share2" data-services="vkontakte,facebook,odnoklassniki,moimir,gplus,twitter,lj"></div>
     			</div>
     			<?php
     		}
