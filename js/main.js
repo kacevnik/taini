345977111363fde0bf81fc593cf0cd0e6ba72417
kacevnik <kacevnik@yandex.ru>
2017-06-30 12:05:59 +0300

    Изменил функционал выпадающего меню

--- js/main.js
+++ js/main.js
@@ -27,9 +27,4 @@ jQuery(document).ready(function ($) {
 	$('img[title=\'LiveInternet: показано число просмотров за 24 часа, посетителей за 24 часа и за сегодня\']').width('0').height('0');
 	$('img[alt=\'Яндекс.Метрика\']').width('0').height('0');
 
-	$('#nav li').hover(function(){
-		$(this).find('.sub-menu').slideDown('400');
-	}, function(){
-		$(this).find('.sub-menu').slideUp('400');
-	});
 });
